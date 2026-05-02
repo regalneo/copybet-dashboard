cdc4b1c5d990fcbf2a77e91811137d2401ef0fb7
Bluey <bluey@copybet.local>
2026-05-02 04:52:50 +0000

    Add 10 wallets (burner-2,3,8,9,10) to dashboard

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -3,32 +3,62 @@ export const COORDINATOR = 'https://api.agentpredict.work';
 export const WALLETS = [
   {
     id: 'burner-1',
-    label: 'BNB Target',
+    label: 'Target 1',
     address: '0x7239853Ef7DEb1a00B3E2583e44b06B29949B756',
     target: '0x8537bfbd08d4d37dd85228169b5d640d625b03a2',
   },
   {
+    id: 'burner-2',
+    label: 'Target 1',
+    address: '0xE1B3ab11261030359b2C1a8747B2fFA74AF043d8',
+    target: '0x8537bfbd08d4d37dd85228169b5d640d625b03a2',
+  },
+  {
+    id: 'burner-3',
+    label: 'Target 2',
+    address: '0x1818661B0e8970d3b615fb74Bde0f3978a6F468c',
+    target: '0xcd0baeb4e8ea89338bebd1470e3e4fe636211cd8',
+  },
+  {
     id: 'burner-4',
-    label: 'SOL Target',
+    label: 'Target 2',
     address: '0x4b3993C0bd5d141419E9EF1164d61eEeaE7563C1',
     target: '0xcd0baeb4e8ea89338bebd1470e3e4fe636211cd8',
   },
   {
     id: 'burner-5',
-    label: 'ETH Target',
+    label: 'Target 3',
     address: '0x52Be0b6e122faA2b6a07Cb1f526ef0540429ad2b',
     target: '0x23b50576710f5d6fe522dc2d89ab5c3342a2ed43',
   },
   {
+    id: 'burner-8',
+    label: 'Target 3',
+    address: '0x4A772Eb0Be1034E04757eBB0544c90d03dcAAFF2',
+    target: '0x23b50576710f5d6fe522dc2d89ab5c3342a2ed43',
+  },
+  {
     id: 'burner-6',
-    label: 'BTC Target',
+    label: 'Target 4',
     address: '0x630Ac8AeCa5E567040B76a6Ee83e49D71055bC9F',
     target: '0xd0b826c4c0f4f83ecc8560b811b818291f876a2e',
   },
   {
+    id: 'burner-9',
+    label: 'Target 4',
+    address: '0x29AC178630430FD5C37EAc2f90d0d0DDf1A8DA40',
+    target: '0xd0b826c4c0f4f83ecc8560b811b818291f876a2e',
+  },
+  {
     id: 'burner-7',
-    label: 'SOL Target',
+    label: 'Target 5',
     address: '0x6092F1BC5990159F69A70F21D08163B76e27F432',
+    target: '0x56786f28b1aaa0463db46d0fedb0c1c52e0bbb66',
+  },
+  {
+    id: 'burner-10',
+    label: 'Target 5',
+    address: '0x336f83668Da33F4E760a466874C60F40Eb3741FD',
     target: '0x56786f28b1aaa0463db46d0fedb0c1c52e0bbb66',
   },
 ];
